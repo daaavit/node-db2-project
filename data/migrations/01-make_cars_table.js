48314b4515ba1eefdf63b67e716e5e32cf3d6f5f
jabaracconich <87547340+jabaracconich@users.noreply.github.com>
2022-02-15 16:27:54 -0500

    b

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,14 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable(cars, tbl => {
+    tbl.increments()
+    tbl.string('vin', 17).notNullable().uniqe()
+    tbl.strin('make', 128).notNullable()
+    tbl.string('model', 128).notNullable()
+    tbl.numeric('milage',).unsigned().notNullable()
+    tbl.string('title', 128)
+    tbl.string('transmission', 128)
+  })
 };
 
 exports.down = function (knex) {
