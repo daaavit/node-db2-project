4b15834bc32ac5049e877a38fb1fdbb773004b32
jabaracconich <87547340+jabaracconich@users.noreply.github.com>
2022-02-15 16:20:22 -0500

    a

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,37 @@
+const { default: knex } = require("knex")
+
 // STRETCH
+const cars = [{
+
+    vin: '11111111111111111',
+    make: 'lexus',
+    model: 'gx',
+    milage: 4333,
+    title: 'clean',
+    transmission: 'automatic'
+
+},
+{
+
+    vin: '11111111111111111',
+    make: 'bmw',
+    model: '535i',
+    milage: 93123,
+    title: 'salvage',
+    transmission: 'manual'
+
+},
+{
+
+    vin: '11111111111111111',
+    make: 'mercedes',
+    model: 'e500',
+    milage: 45333,
+
+}]
+
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
